d0b7d4a5bb3a3f2634f2c91064e62b7792cf45d6
Florian Scholz <florian90@gmail.com>
2018-08-21 15:25:57 +0200

    Cleaning up code

--- pcf8591.c
+++ pcf8591.c
@@ -37,7 +37,6 @@ static struct i2c_board_info info_30 = {
 
 static void drive_reset(struct work_struct *data)
 {
-        //TODO: drive Treiber auf 0 0 setzen --> Stillstand 
         char send_buffer[2];
 
         printk("drive_reset is called\n");
@@ -79,21 +78,6 @@ static int driver_close( struct inode *geraete_datei, struct file *instanz )
 	return 0;
 }
 
-static ssize_t driver_read( struct file *instanz,
-	char __user *user, size_t count, loff_t *offset )
-{
-	unsigned long not_copied, to_copy;
-	char value;
-
-	// TODO: HARDWARE-ZUGRIFFE UEBER I2C ERGAENZEN
-	// .... Messung starten
-	// .... Wert auslesen
-	//
-
-	to_copy = min( count, sizeof(value) );
-	not_copied=copy_to_user( user, &value, to_copy);
-	return to_copy - not_copied;
-}
 
 static ssize_t driver_write( struct file *instanz, const char __user *user,
   size_t count, loff_t *offset )
@@ -109,8 +93,7 @@ static ssize_t driver_write( struct file *instanz, const char __user *user,
 	to_copy = min( count, sizeof(send_buffer)-1);
 	not_copied = copy_from_user(&send_buffer[1], user, to_copy);
 
-	if (send_buffer[1]==0) { // XXX - muss noch genauer angesehen werden...
-		//drive_reset(NULL);
+	if (send_buffer[1]==0) {
 		return count;
 	}
 
@@ -144,10 +127,7 @@ static int pcf8591_probe( struct i2c_client *client,
 		printk("i2c_probe %x: not found\n",client->addr);
 		return -1;
 	}
-	// TODO: KONFIGUATION DES PCF8591
-	// Analog channel 0, no auto increment, analog out enable
-	// In welcher Variablen wird der Wert uebergeben?
-	// buf = ?????
+	
 	buf[0] = 0x40;
 	buf[1] = 0x80;
 	i2c_master_send( client, buf, 2 );
@@ -160,7 +140,6 @@ static int pcf8591_remove( struct i2c_client *client )
 }
 static struct file_operations fops = {
 	.owner= THIS_MODULE,
-	.read = driver_read,
 	.write = driver_write,
 	.open = driver_open,
 	.release = driver_close
@@ -179,25 +158,25 @@ static int __init mod_init( void )
 {
 	if( alloc_chrdev_region(&pcf8591_dev_number,0,2,"pcf8591")<0 )
 		return -EIO;
-printk("1\n");
+	
 	driver_object = cdev_alloc();
 	if( driver_object==NULL )
 		goto free_device_number;
 	driver_object->owner = THIS_MODULE;
 	driver_object->ops = &fops;
-printk("2\n");
+	
 	if( cdev_add(driver_object,pcf8591_dev_number,1) )
 		goto free_cdev;
 
 	if( cdev_add(driver_object,pcf8591_dev_number+1,1) )
 		goto free_cdev;
 	pcf8591_class = class_create( THIS_MODULE, "pcf8591" );
-printk("3\n");
+	
 	if( IS_ERR( pcf8591_class ) ) {
 		pr_err( "pcf8591: no udev support\n");
 		goto free_cdev;
 	}
-printk("4\n");
+	
 	pcf8591a_dev = device_create( pcf8591_class, NULL,
 	pcf8591_dev_number, NULL, "%s", "dac-1" );
 	if(pcf8591a_dev == NULL) 
@@ -205,10 +184,9 @@ printk("4\n");
 		pr_err("device_create(): epic fail\n");
 		goto destroy_dev_class;
 	}
-printk("5\n");
+	
 	dev_info(pcf8591a_dev, "mod_init\n");
 
-printk("6\n");
 	pcf8591b_dev = device_create( pcf8591_class, NULL,
 	pcf8591_dev_number+1, NULL, "%s", "dac-2" );
 	if(pcf8591b_dev == NULL) {
@@ -216,24 +194,24 @@ printk("6\n");
 		goto destroy_dev_class;
 	}
 	dev_info(pcf8591b_dev, "mod_init\n");
-printk("7\n");
+	
 	if (i2c_add_driver(&pcf8591_driver)) {
 		pr_err("i2c_add_driver failed\n");
 		goto destroy_dev_class;
 	}
-printk("8\n");
+	
 	adapter = i2c_get_adapter(1); // Adapter sind durchnummeriert
 	if (adapter==NULL) {
 		pr_err("i2c_get_adapter failed\n");
 		goto del_i2c_driver;
 	}
-	printk("9\n");
+	
 	slave_01 = i2c_new_device( adapter, &info_20 );
 	if (slave_01==NULL) {
 		pr_err("i2c_new_device failed\n");
 		goto del_i2c_driver;
 	}
-	printk("10\n");
+
 	slave_02 = i2c_new_device( adapter, &info_30 );
 
 	if(slave_02 == NULL) {
